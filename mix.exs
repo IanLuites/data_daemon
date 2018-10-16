4763ab0c8e74fc0391e99d2b07caa5b8d3bcd9e4
Ian Luites <IanLuites@users.noreply.github.com>
2018-10-16 12:13:23 +0200

    Release v0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DataDaemon.MixProject do
   def project do
     [
       app: :data_daemon,
-      version: "0.1.2",
+      version: "0.1.3",
       description: "An Elixir StatsD client made for DataDog.",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
