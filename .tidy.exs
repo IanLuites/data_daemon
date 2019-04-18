7e452ce60c46228a1ce0acba82cbb88fb3680fb0
Ian Luites <IanLuites@users.noreply.github.com>
2019-04-18 08:40:55 +0200

    Clean dependencies.

--- .tidy.exs
+++ .tidy.exs
@@ -10,7 +10,19 @@
     {Tidy.Checks.ModuleDoc, [level: :error]}
   ],
   ignore: %{
-    functions: [__struct__: 0, __struct__: 1, __changeset__: 0, __schema__: 1, __schema__: 2],
+    functions: [
+      __struct__: 0,
+      __struct__: 1,
+      __changeset__: 0,
+      __schema__: 1,
+      __schema__: 2,
+      child_spec: 0,
+      child_spec: 1,
+      code_change: 3,
+      handle_call: 3,
+      handle_cast: 2,
+      terminate: 2
+    ],
     modules: [
       DataDaemon.Hound
     ]
