f1b4dfd07959d9d0b23389005c41c883d0caf295
Ian Luites <IanLuites@users.noreply.github.com>
2020-08-11 18:04:59 +0200

    Travis does not like localhost.

--- test/data_daemon_test.exs
+++ test/data_daemon_test.exs
@@ -7,7 +7,7 @@ defmodule DataDaemonTest do
 
   test "metric/5" do
     Supervisor.start_link([Example2], strategy: :one_for_one)
-    DataDaemon.Hound.open(Example2, 'localhost', 8125)
+    DataDaemon.Hound.open(Example2, '127.0.0.1', 8125)
     assert DataDaemon.metric(Example2, "example", 5, :counter) == :ok
   end
 end
